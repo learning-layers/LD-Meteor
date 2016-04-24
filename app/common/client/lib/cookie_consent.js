632d74979a13d684f5671ebc3c3b216cbd699a8e
Martin Bachl <martin@bachl.pro>
2016-04-24 16:45:07 +0200

    - Added preliminary cookie consent form.

---
+++ app/common/client/lib/cookie_consent.js
@@ -0,0 +1,33 @@
+var options = {
+  cookieTitle: "We use Cookies",
+  cookieMessage: "We are using cookies to give you the best"
+  + " experience on our site. Cookies are files stored in your"
+  + " browser and are used by most websites to help personalise your web experience.",
+  showLink: true,
+  linkText: "Read more",
+  linkRouteName: "/cookiePolicy",
+  acceptButtonText: "Accept and Continue",
+  html: false,
+  expirationInDays: 7
+};
+
+CookieConsent.init(options);
+
+// or
+
+var optionsImply = {
+  cookieMessage: "We are using cookies to give you the best"
+  + " experience on our site. Cookies are files stored in your"
+  + " browser and are used by most websites to help personalise your web experience.",
+  cookieMessageImply: "By continuing to use our website,"
+  + " you are agreeing to our use of cookies.",
+  showLink: true,
+  position: 'top',
+  linkText: "",
+  linkRouteName: "/cookiePolicy",
+  html: false,
+  className: null,
+  expirationInDays: 7
+};
+
+CookieConsent.init(optionsImply);
--- app/common/client/ui/mainLayout/MainLayout.js
+++ app/common/client/ui/mainLayout/MainLayout.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 
-const MainLayout = ({content, header}) => (
-  <div>
-    <header>
-      {header}
-    </header>
-    <main>
-      {content}
-    </main>
-  </div>
-);
+class MainLayout extends Component {
+  componentDidMount() {
+    // Use Meteor Blaze to render the consent form
+    this.view = Blaze.render(Template.cookieConsentImply,
+      ReactDOM.findDOMNode(this.refs.cookieConsentForm));
+    setTimeout(function() {
+      Blaze.remove(this.view);
+    }, 30000);
+  }
+  componentWillUnmount() {
+    // Clean up Blaze view
+    Blaze.remove(this.view);
+  }
+  render() {
+    return <div>
+      <div ref="cookieConsentForm"></div>
+      <header>
+        {this.props.header}
+      </header>
+      <main>
+        {this.props.content}
+      </main>
+    </div>
+  }
+}
 
 export default MainLayout;
