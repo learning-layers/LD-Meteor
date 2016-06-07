a3826ad447998789c5509e10c085da02f3be8a39
Aur0r <martin.bachl@web.de>
2016-06-07 13:02:27 +0200

    Added settings button and glyphicons to the user navigation.

--- app/common/client/ui/mainLayout/additionalNavbarButtons.js
+++ app/common/client/ui/mainLayout/additionalNavbarButtons.js
@@ -3,5 +3,8 @@ import { Template } from 'meteor/templating'
 Template._loginButtonsLoggedInDropdown.events({
   'click #login-buttons-edit-profile': function (event) {
     global.emitter.emit('sidebar-toggle', false)
+  },
+  'click #login-buttons-edit-settings': function (event) {
+    global.emitter.emit('sidebar-toggle', false)
   }
 })
