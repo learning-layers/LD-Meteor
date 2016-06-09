657fa44f06e793ed31d9985f1e226722cbc93433
Aur0r <martin.bachl@web.de>
2016-06-09 14:14:53 +0200

    Redesigned the access forbidden page.

--- app/common/client/ui/mainLayout/MainLayout.js
+++ app/common/client/ui/mainLayout/MainLayout.js
@@ -107,7 +107,17 @@ class MainLayout extends Component {
                   registeredEmails={user.registered_emails} />
               )}
             </div>)}
-          </div> : <div>You are not allowed to access this route</div>}
+          </div> : <div className='container'>
+            <div className='panel panel-danger'>
+              <div className='panel-heading'><h2>Access Forbidden</h2></div>
+              <div className='panel-body'>
+                <div className='access-forbidden-panel-content-wrapper'>
+                  <img src='/stop-sign-35069.svg' alt='Stop Sign' />
+                  You don't have permission to be here...
+                </div>
+              </div>
+            </div>
+          </div>}
           {this.props.helpCenter}
         </main>
         <LDSidebar />
