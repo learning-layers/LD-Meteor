17329aa8f4f0dd5f6e9165f0427248ead0a959cf
Aur0r <martin@bachl.pro>
2016-06-30 15:26:25 +0200

    Added etherpad controller.

---
+++ app/packages/etherpad/server/EtherpadController.js
@@ -0,0 +1,104 @@
+import { HTTP } from 'meteor/http'
+import { Meteor } from 'meteor/meteor'
+
+class EtherpadController {
+  constructor () {
+    this.endpoint = Meteor.settings.private.etherpad.endpoint
+    console.log('Etherpad connection is set to endpoint=' + this.endpoint)
+    this.apiKey = Meteor.settings.private.etherpad.apiKey
+  }
+  createGroup (callback) {
+    HTTP.post(this.endpoint + '/createGroup', {
+      data: {
+        'apikey': this.apiKey
+      }
+    }, function (error, response) {
+      if (error) {
+        console.log(error)
+        callback(error)
+      } else {
+        if (response.data.code === 0) {
+          // ok status
+          console.log(response.data.data.groupID)
+          callback(null, response.data.data.groupID)
+        } else {
+          // TODO add error handling
+          callback('error')
+        }
+      }
+    })
+  }
+  createGroupPad (groupId, title, callback) {
+    // TODO const document = Documents.findOne({'_id': documentId}, {fields: {etherpad_group: 1}})
+    HTTP.post(this.endpoint + '/createGroupPad', {
+      data: {
+        'apikey': this.apiKey,
+        'groupID': groupId,
+        'padName': title
+      }
+    }, function (error, response) {
+      if (error) {
+        console.log(error)
+        callback(error)
+      } else {
+        if (response.data.code === 0) {
+          // ok status
+          console.log(response.data.data.padID)
+          callback(null, response.data.data.padID)
+        } else {
+          // TODO add error handling
+          callback('error')
+        }
+      }
+    })
+  }
+  createAuthor (userId, userName, callback) {
+    HTTP.post(this.endpoint + '/createAuthorIfNotExistsFor', {
+      data: {
+        'apikey': this.apiKey,
+        'name': userName,
+        'authorMapper': userId
+      }
+    }, function (error, response) {
+      if (error) {
+        console.log(error)
+        callback(error)
+      } else {
+        if (response.data.code === 0) {
+          // ok status
+          console.log(response.data.data.authorID)
+          callback(null, response.data.data.authorID)
+        } else {
+          // TODO add error handling
+          callback('error')
+        }
+      }
+    })
+  }
+  createPadSession (groupId, authorId, validUntil, callback) {
+    HTTP.post(this.endpoint + '/createSession', {
+      data: {
+        'apikey': this.apiKey,
+        'groupID': groupId,
+        'authorID': authorId,
+        'validUntil': validUntil
+      }
+    }, function (error, response) {
+      if (error) {
+        console.log(error)
+        callback(error)
+      } else {
+        if (response.data.code === 0) {
+          // ok status
+          console.log(response.data.data.sessionID)
+          callback(null, response.data.data.sessionID)
+        } else {
+          // TODO add error handling
+          callback('error')
+        }
+      }
+    })
+  }
+}
+
+export const EtherpadControllerInstance = new EtherpadController()
