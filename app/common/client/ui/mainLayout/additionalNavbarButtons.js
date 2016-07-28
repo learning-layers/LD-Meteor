dc407d715fd5790929e5cd2d50eb4fd76db89b06
Aur0r <martin@bachl.pro>
2016-07-28 16:24:44 +0200

    The manage groups button now closes the sidebar upon clicking it.

--- app/common/client/ui/mainLayout/additionalNavbarButtons.js
+++ app/common/client/ui/mainLayout/additionalNavbarButtons.js
@@ -4,7 +4,7 @@ Template._loginButtonsLoggedInDropdown.events({
   'click #login-buttons-edit-profile': function (event) {
     global.emitter.emit('sidebar-toggle', false)
   },
-  'click #login-buttons-edit-settings': function (event) {
+  'click #login-buttons-edit-groups': function (event) {
     global.emitter.emit('sidebar-toggle', false)
   }
 })
