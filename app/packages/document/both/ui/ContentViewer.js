c37d4e583d46af8dda47e92aa662448d7bfa99ba
Aur0r <martin@bachl.pro>
2016-08-06 01:24:50 +0200

    Added ssr for the etherpad content.

--- app/packages/document/both/ui/ContentViewer.js
+++ app/packages/document/both/ui/ContentViewer.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import { Meteor } from 'meteor/meteor'
+import { Documents } from '../../../document/lib/collections'
 
 class ContentViewer extends Component {
   constructor (props) {
@@ -23,17 +24,47 @@ class ContentViewer extends Component {
     })
   }
   render () {
+    const document = Documents.findOne({ '_id': this.props.documentId }, { etherpadGroupPad: 1 })
+    if (Meteor.isServer) {
+      let serverHtml, serverError
+      let getEtherpadHtmlContentSync = Meteor.wrapAsync(Meteor.call)
+      if (document) {
+        try {
+          let result = getEtherpadHtmlContentSync('getEtherpadHtmlContent', this.props.documentId, this.props.accessKey, (err, res) => {
+            if (err) {
+              serverError = err
+            } else {
+              serverHtml = res
+            }
+          })
+          console.log(result)
+        } catch (e) {
+          serverError = e
+        }
+        if (serverError) {
+          return <div id='content-viewer'>
+            Error: {JSON.stringify(serverError)}
+          </div>
+        } else {
+          return <div id='content-viewer'>
+            <div dangerouslySetInnerHTML={{__html: serverHtml}}></div>
+          </div>
+        }
+      }
+    }
     if (!this.state.htmlLoaded && !this.state.error) {
       // send a request to the server that the server should return the etherpad content as html
-      Meteor.setTimeout(() => {
-        this.loadHtml()
-      }, 0)
+      if (Meteor.isClient) {
+        Meteor.setTimeout(() => {
+          this.loadHtml()
+        }, 0)
+      }
       return <div id='content-viewer'>
         loading...
       </div>
     } else if (this.state.error) {
       return <div id='content-viewer'>
-        {JSON.stringify(this.state.error)}
+        Error: {JSON.stringify(this.state.error)}
       </div>
     } else {
       return <div id='content-viewer'>
--- app/packages/etherpad/server/EtherpadController.js
+++ app/packages/etherpad/server/EtherpadController.js
@@ -1,5 +1,6 @@
 import { HTTP } from 'meteor/http'
 import { Meteor } from 'meteor/meteor'
+import { Documents } from '../../document/lib/collections'
 
 class EtherpadController {
   constructor () {
@@ -137,13 +138,18 @@ class EtherpadController {
       }
     })
   }
-  getHTMLContent (groupPadId, callback) { // get the html representation of the etherpad content
+  getHTMLContentByDocumentId (documentId, callback) { // get the html representation of the etherpad content
+    console.log('BEGIN getHTMLContent')
+    console.log('getHTMLContent> documentId=' + documentId)
+    const document = Documents.findOne({ '_id': this.props.documentId }, { etherpadGroupPad: 1 })
+    console.log(document)
     HTTP.post(this.endpoint + '/getHTML', {
       data: {
         'apikey': this.apiKey,
-        'padID': groupPadId
+        'padID': document.etherpadGroupPad
       }
     }, function (error, response) {
+      console.log(arguments)
       if (error) {
         console.log(error)
         callback(error)
@@ -153,9 +159,39 @@ class EtherpadController {
           callback(null, response.data.data.html)
         } else {
           // TODO add error handling
+          console.log(response)
           callback('error')
         }
       }
+      console.log('END getHTMLContent')
+    })
+  }
+  getHTMLContent (groupPadId, callback) { // get the html representation of the etherpad content
+    console.log('BEGIN getHTMLContent')
+    console.log('getHTMLContent> groupPadId=' + groupPadId)
+    HTTP.post(this.endpoint + '/getHTML', {
+      data: {
+        'apikey': this.apiKey,
+        'padID': groupPadId
+      }
+    }, function (error, response) {
+      console.log(arguments)
+      if (error) {
+        console.log(error)
+        callback(error)
+      } else {
+        console.log('response.data.code=', response.data.code)
+        if (response.data.code === 0) {
+          // ok status
+          console.log('ok status')
+          callback(null, response.data.data.html)
+        } else {
+          // TODO add error handling
+          console.log(response)
+          callback('error')
+        }
+      }
+      console.log('END getHTMLContent')
     })
   }
   getReadOnlyPadId (groupPadId, callback) {
