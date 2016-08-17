c94f6485c7ab2db7b5220ebf6647262b5454cb3a
Aur0r <martin@bachl.pro>
2016-08-17 11:59:10 +0200

    Added messaging window.

--- app/packages/chat/client/ui/FriendList.js
+++ app/packages/chat/client/ui/FriendList.js
@@ -8,6 +8,7 @@ import { FriendRequests, FriendLists } from '../../lib/collections'
 import OpenFriendRequests from './OpenFriendRequests'
 import InnerFriendList from './InnerFriendList'
 import Button from '../../../../../node_modules/react-bootstrap/lib/Button'
+import EventEmitterInstance from '../../../../common/client/EventEmitter'
 
 class ActiveFilterTestComp extends Component {
   render () {
@@ -17,6 +18,32 @@ class ActiveFilterTestComp extends Component {
 
 ActiveFilterTestComp.propTypes = {
   activeFilter: React.PropTypes.bool
+}
+
+class FriendChat extends Component {
+  close () {
+    EventEmitterInstance.emit('close-friend-small-chat')
+  }
+  render () {
+    const { friendId } = this.props
+    return <div id='small-friend-chat' style={{position: 'relative'}}>
+      {friendId}
+      <div style={{
+        position: 'absolute',
+        top: '5px',
+        right: '5px',
+        fontWeight: 'bold',
+        fontSize: '30px',
+        cursor: 'pointer'
+      }} onClick={() => this.close()}>
+        &times;
+      </div>
+    </div>
+  }
+}
+
+FriendChat.propTypes = {
+  friendId: React.PropTypes.string
 }
 
 function onPropsChange (props, onData) {
@@ -31,11 +58,52 @@ function onPropsChange (props, onData) {
   }
 }
 
+class FriendContainer extends Component {
+  render () {
+    const { friendList, openFriendRequests } = this.props
+    return <div className='friend-container'>
+      {openFriendRequests && openFriendRequests.length > 0 ? (
+        <OpenFriendRequests
+          openFriendRequests={openFriendRequests}
+          friendRequestsLoading={this.props.friendRequestsLoading} />
+      ) : null}
+      <CollapsibleFilterContainer alwaysOpen filters={['All', 'Online', 'History']} activeFilter={'Online'}>
+        <InnerFriendList friendList={friendList} />
+      </CollapsibleFilterContainer>
+    </div>
+  }
+}
+
+FriendContainer.propTypes = {
+  friendRequestsLoading: React.PropTypes.bool,
+  openFriendRequests: React.PropTypes.array,
+  friendList: React.PropTypes.object
+}
+
+const FriendContainerWithData = composeWithTracker(onPropsChange)(FriendContainer)
+
 class FriendList extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      openAddFriendModal: null
+      openAddFriendModal: null,
+      friendChat: null
+    }
+  }
+  componentDidMount () {
+    this.smallChatWindowOpenSubscription = EventEmitterInstance.addListener('open-friend-small-chat', (friendId) => {
+      this.setState({friendChat: friendId})
+    })
+    this.smallChatWindowCloseSubscription = EventEmitterInstance.addListener('close-friend-small-chat', () => {
+      this.setState({friendChat: null})
+    })
+  }
+  componentWillUnmount () {
+    if (this.smallChatWindowOpenSubscription) {
+      this.smallChatWindowOpenSubscription.remove()
+    }
+    if (this.smallChatWindowCloseSubscription) {
+      this.smallChatWindowCloseSubscription.remove()
     }
   }
   openAddFriendModal () {
@@ -47,8 +115,6 @@ class FriendList extends Component {
     }
   }
   render () {
-    const { friendList, openFriendRequests } = this.props
-    console.log(openFriendRequests)
     return <div className='ld-friendlist'>
       <div className='top-bar'>
         <Button bsSize='small' onClick={() => this.openAddFriendModal()}>
@@ -59,24 +125,9 @@ class FriendList extends Component {
         <div ref='addFriendModal'></div>
       </div>
       <hr className='no-margin' />
-      <div className='friend-container'>
-        {openFriendRequests && openFriendRequests.length > 0 ? (
-          <OpenFriendRequests
-            openFriendRequests={openFriendRequests}
-            friendRequestsLoading={this.props.friendRequestsLoading} />
-        ) : null}
-        <CollapsibleFilterContainer alwaysOpen filters={['All', 'Online', 'History']} activeFilter={'Online'}>
-          <InnerFriendList friendList={friendList} />
-        </CollapsibleFilterContainer>
-      </div>
+      {this.state.friendChat === null ? <FriendContainerWithData /> : <FriendChat friendId={this.state.friendChat} />}
     </div>
   }
 }
 
-FriendList.propTypes = {
-  friendRequestsLoading: React.PropTypes.bool,
-  openFriendRequests: React.PropTypes.array,
-  friendList: React.PropTypes.object
-}
-
-export default composeWithTracker(onPropsChange)(FriendList)
+export default FriendList
--- app/packages/chat/client/ui/InnerFriendList.js
+++ app/packages/chat/client/ui/InnerFriendList.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import { Meteor } from 'meteor/meteor'
+import EventEmitterInstance from '../../../../common/client/EventEmitter'
 
 class FriendListGroup extends Component {
   render () {
@@ -15,6 +16,25 @@ FriendListGroup.propTypes = {
   name: React.PropTypes.string
 }
 
+class Friend extends Component {
+  openSmallChatWindow (friendId) {
+    EventEmitterInstance.emit('open-friend-small-chat', friendId)
+  }
+  render () {
+    const { friend } = this.props
+    return <li>
+      <span>{friend.profile.name}</span>
+      <button className='btn btn-default' onClick={() => this.openSmallChatWindow(friend._id)}>
+        <span className='glyphicon glyphicon-comment' />
+      </button>
+    </li>
+  }
+}
+
+Friend.propTypes = {
+  friend: React.PropTypes.object
+}
+
 class InnerFriendList extends Component {
   render () {
     const { activeFilter, friendList } = this.props
@@ -24,7 +44,11 @@ class InnerFriendList extends Component {
         <ul>
           {friendList.friendIds.map(function (friendId) {
             let friend = Meteor.users.findOne({_id: friendId})
-            return <li>{friend ? friend.profile.name : friendId}</li>
+            if (friend) {
+              return <Friend key={'friend-' + friend._id} friend={friend} />
+            } else {
+              return null
+            }
           })}
           {friendList.groups.map(function (group) {
             return <FriendListGroup groupId={group._id} name={group.name} />
