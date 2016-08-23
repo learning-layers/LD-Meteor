9740639b8f4da725b095c44a7b1a3207a8a7005f
Aur0r <martin@bachl.pro>
2016-08-23 19:31:09 +0200

    Added history layout.

--- app/packages/document/client/ui/mainContent/history/HistoryArea.js
+++ app/packages/document/client/ui/mainContent/history/HistoryArea.js
@@ -3,7 +3,24 @@ import React, {Component} from 'react'
 class HistoryArea extends Component {
   render () {
     return <div id='history'>
-      {'History'}
+      <ul className='timeline'>
+        <li key={'tl-item-1'} className='tl-item'>
+          <div className='content'>
+            <div className='lbl'>Test</div>
+            <span className='date'>12 May 2013</span>
+            <span className='circle'></span>
+          </div>
+          <div className='placeholder'></div>
+        </li>
+        <li key={'tl-item-2'} className='tl-item'>
+          <div className='content'>
+            <div className='lbl'>Test2</div>
+            <span className='date'>12 May 2013</span>
+            <span className='circle'></span>
+          </div>
+          <div className='placeholder'></div>
+        </li>
+      </ul>
     </div>
   }
 }
