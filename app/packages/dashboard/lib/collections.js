11e542e5c4be37b8efad9f190413098941b386c5
Aur0r <martin.bachl@web.de>
2016-09-30 15:14:57 +0200

    Adjusted indexes

--- app/packages/dashboard/lib/collections.js
+++ app/packages/dashboard/lib/collections.js
@@ -1,5 +1,10 @@
 import { Mongo } from 'meteor/mongo'
+import { Meteor } from 'meteor/meteor'
 import { UserPositionSchema } from './schema'
 
 export const UserPositions = new Mongo.Collection('UserPositions')
 UserPositions.attachSchema(UserPositionSchema)
+if (Meteor.isServer) {
+  UserPositions._ensureIndex({ userId: 1 })
+  UserPositions._ensureIndex({ type: 1, elementId: 1 })
+}
