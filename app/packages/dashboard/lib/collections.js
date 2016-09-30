f0761415051099324f51fed979539e2ac12930ef
Aur0r <martin.bachl@web.de>
2016-09-30 21:04:17 +0200

    Added UserActivityHistory collection.

--- app/packages/dashboard/lib/collections.js
+++ app/packages/dashboard/lib/collections.js
@@ -1,10 +1,16 @@
 import { Mongo } from 'meteor/mongo'
 import { Meteor } from 'meteor/meteor'
-import { UserPositionSchema } from './schema'
+import { UserPositionSchema, UserActivityHistorySchema } from './schema'
 
 export const UserPositions = new Mongo.Collection('UserPositions')
 UserPositions.attachSchema(UserPositionSchema)
 if (Meteor.isServer) {
   UserPositions._ensureIndex({ userId: 1 })
   UserPositions._ensureIndex({ type: 1, elementId: 1 })
+}
+
+export const UserActivityHistory = new Mongo.Collection('UserActivityHistory')
+UserActivityHistory.attachSchema(UserActivityHistorySchema)
+if (Meteor.isServer) {
+  UserActivityHistory._ensureIndex({userId: 1, type: 1})
 }
--- app/packages/dashboard/lib/schema.js
+++ app/packages/dashboard/lib/schema.js
@@ -20,3 +20,38 @@ export const UserPositionSchema = new SimpleSchema({
     min: 6
   }
 })
+
+export const UserActivityHistorySchema = new SimpleSchema({
+  userId: {
+    type: String,
+    label: 'UserId',
+    max: 40,
+    min: 6
+  },
+  type: {
+    type: String,
+    label: 'Type',
+    max: 10,
+    min: 3
+  },
+  elementId: {
+    type: String,
+    label: 'UserId',
+    max: 40,
+    min: 6
+  },
+  action: {
+    type: String,
+    label: 'Action',
+    max: 5000,
+    min: 3
+  },
+  createdAt: {
+    type: Date,
+    label: 'Created at'
+  },
+  values: {
+    type: [String],
+    label: 'Values'
+  }
+})
